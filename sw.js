af56e32b541b1504b1163aad8b528a15edd6462d
Sai Ayshwarya Lakshmi <SaiAyshwaryaLakshmi@users.noreply.github.com>
2019-10-09 15:31:00 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,34 @@
-var cachename = 'hello-pwa';
+var cacheName = 'hello-pwa';
 var filesToCache = [
-'/',
-'/tones',
-'/comments',
-'app.js',
-'manifest.json',
-'views/index.ejs',
-'views/tones.ejs'
-'views/comments.ejs',
-'css/style.css'
-'main.js'];
+  '/',
+  '/tones',
+  '/comments',
+  'app.js',
+  'manifest.json',
+  'views/index.ejs',
+  'views/tones.ejs',
+  'views/comments.ejs',
+  'css/style.css',
+  'js/main.js'
+];
 
-self.addEventListener('install',function(e) {
-e.waitUntil(caches.open(cacheName).then(function(cache){
-   return cache.addAll(filesToCache);})
-);
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+    console.log("installing");
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
-self.addEventListener('fetch',function(e) {
-e.respondWith(caches.match(e.request).then(function(response){
-return response || fetch(e.request);
-})
-);
+
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
